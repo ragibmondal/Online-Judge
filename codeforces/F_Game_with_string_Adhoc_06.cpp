ef8ac1be34215dbbcf46d46d577c4990feb5b2fb
Ragib Hasan <hasan2305101775@diu.edu.bd>
2024-09-20 23:56:51 +0600

    CP

---
+++ codeforces/F_Game_with_string_Adhoc_06.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    //vjudge user: gabrielisenberg
+    string s;
+    cin >> s;
+    int wins = 0;
+    stack<char> letters;
+    
+    for (char c : s) {
+        if (!letters.empty() && letters.top() == c) {
+            wins++;
+            letters.pop();
+        } else {
+            letters.push(c);
+        }
+    }
+    
+    wins % 2 == 0 ? cout << "No" << endl : cout << "Yes" << endl;
+    return 0;
+}
