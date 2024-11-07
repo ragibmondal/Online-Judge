0c52a61ba4f3e8ccd54f2344f135180337ee71c9
ragibmondal <hasan2305101775@diu.edu.bd>
2024-11-07 22:17:22 +0600

    cp

---
+++ codeforces/C_Compare.cpp
@@ -0,0 +1,22 @@
+/***************************************
+Author: Ragib Hasan
+Date: 2024-11-07 22:13:03
+****************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+       string X, Y;
+    cin >> X >> Y;
+
+    if (X < Y) {
+        cout << X << endl;
+    } else {
+        cout << Y << endl;
+    }
+
+    return 0;
+    return 0;
+}
